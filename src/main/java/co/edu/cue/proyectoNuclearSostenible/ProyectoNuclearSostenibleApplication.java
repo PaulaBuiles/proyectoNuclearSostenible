5c668cbabba69984819ac1dd408116da815fc06a
PAULA ANDREA BUILES <paula.builes@answercpi.com>
2024-04-24 08:13:04 -0500

    Initial commit

---
+++ src/main/java/co/edu/cue/proyectoNuclearSostenible/ProyectoNuclearSostenibleApplication.java
@@ -0,0 +1,13 @@
+package co.edu.cue.proyectoNuclearSostenible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNuclearSostenibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNuclearSostenibleApplication.class, args);
+	}
+
+}
