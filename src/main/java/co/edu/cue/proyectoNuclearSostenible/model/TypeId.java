b94409c12810919035f4cbc178f22a83bf74911e
PAULA ANDREA BUILES <paula.builes@answercpi.com>
2024-04-25 12:28:48 -0500

    feat(Se crean las primeras clases con FK)

---
+++ src/main/java/co/edu/cue/proyectoNuclearSostenible/model/TypeId.java
@@ -0,0 +1,35 @@
+package co.edu.cue.proyectoNuclearSostenible.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "type_id", indexes = @Index(columnList = "idTypeId"))
+public class TypeId implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTypeId;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @OneToMany(mappedBy = "typeId", cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    private Set<User> listUsers = new HashSet<User>();
+
+
+
+}
--- src/main/java/co/edu/cue/proyectoNuclearSostenible/model/User.java
+++ src/main/java/co/edu/cue/proyectoNuclearSostenible/model/User.java
@@ -12,23 +12,46 @@ public class User {
     private Long id;
     @Column(name = "userName", nullable = false)
     private String userName;
+
     @Column(name = "password", nullable = false)
     private String password;
+
     @Column(name = "fullName", nullable = false)
     private String fullName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "id_type_id")
+    @NotNull(message = "El tipo de identifiación no puede ser nulo o vacio")
+    private TypeId typeIdUser;
+
+    @Column(name = "identification", nullable = false)
+    private String identification;
+
     @Column(name = "image", nullable = false)
     private String image;
+
     @Column(name = "description", nullable = false)
     private String description;
+
     @Column(name = "status", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 1")
     private Boolean status;
 
+    @Column(name = "isEstudent", nullable = false)
+    private Boolean isEstudent;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Product> listProducts = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Product> listShopping = new HashSet<>();
+
+
+    //Usuario tiene listdado de productos, publicaciones y compras
+
 }
